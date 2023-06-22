c6f084ef77f2b26ebdccbfe5a502cb3ac872b3af
Carlos Diaz <carlos.cedp@gmail.com>
2023-06-21 21:21:08 -0400

    add Ioc layer to configure dependency injection

--- IoC/DependencyInjection.cs
+++ IoC/DependencyInjection.cs
@@ -1,17 +1,23 @@
-﻿using Infrastructure.Data;
+﻿using Application.Repositories;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
-namespace Infrastructure
+namespace IoC
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddIfraServices(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddDependencyInjection(this IServiceCollection services, IConfiguration configuration)
         {
+            // db context
             var connectionString = configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             services.AddDbContext<AppDbContext>(db => db.UseSqlServer(connectionString));
-
+            // generic dependencies
+            services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
             return services;
         }
     }
