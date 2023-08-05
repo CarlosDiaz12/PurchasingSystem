59027f76c4efaeac6dcc338aa2d88d2d2109f5ee
Carlos Diaz <carlos.cedp@gmail.com>
2023-08-05 16:51:21 -0400

    se agregó migracion de los dos campos de purchase order

---
+++ Infrastructure/Migrations/20230805205033_add_fields_purchase_order.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_fields_purchase_order : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Total",
+                table: "PurchaseOrders",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "UnitCost",
+                table: "PurchaseOrders",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Total",
+                table: "PurchaseOrders");
+
+            migrationBuilder.DropColumn(
+                name: "UnitCost",
+                table: "PurchaseOrders");
+        }
+    }
+}
