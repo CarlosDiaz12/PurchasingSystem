f7deb3d468f54a76acfe4abbd6115de583fc2e63
Carlos Diaz <carlos.cedp@gmail.com>
2023-08-06 11:06:11 -0400

    add migration

---
+++ Infrastructure/Migrations/20230806150451_add_indetificacion_type.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_indetificacion_type : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "IdentificationType",
+                table: "Suppliers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IdentificationType",
+                table: "Suppliers");
+        }
+    }
+}
